fabffcfdc5352e92084ebc8f6a9b1c6dea4eb0ac
snillocmot <tomcollins@globo-smart.com>
2014-07-15 08:58:01 -0500

    Adds bash setup files for shield and no shield demo's

---
+++ setupGrovePi.sh
@@ -0,0 +1,11 @@
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo easy_install -U distribute
+git clone https://github.com/DexterInd/GrovePi 
+cd GrovePi/Script
+sudo chmod +x install.sh
+sudo ./install.sh
+cd ..
+cd ..
+sudo pip install RPi.GPIO
+sudo pip install paho-mqtt
---
+++ setupRawGPIO.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo easy_install -U distribute
+sudo pip install RPi.GPIO
+sudo pip install paho-mqtt
