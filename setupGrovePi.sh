47b1cc10ea705a571ee541e24ab5c07be7fc39f2
Jan Bogaerts <jb@allthingstalk.com>
2014-11-03 17:30:51 +0100

    removes grovepi.py & updates setupgrovepi.sh
    Gets latest version of grovepi.py  from the freshly installed grovepi
    library.

--- setupGrovePi.sh
+++ setupGrovePi.sh
@@ -3,6 +3,7 @@ sudo apt-get -y install python-pip
 sudo pip install RPi.GPIO
 sudo pip install paho-mqtt
 git clone https://github.com/DexterInd/GrovePi 
+sudo cp /home/pi/GrovePi/Software/Python/grovepi.py /home/pi/gif_python
 cd GrovePi/Script
 sudo chmod +x install.sh
 sudo ./install.sh
