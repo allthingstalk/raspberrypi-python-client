752cf00ecf4b8acc092b83f3a5fc0e88782be96e
Jan Bogaerts <jb@allthingstalk.com>
2014-11-04 16:06:19 +0100

    Update setupGrovePi.sh

--- setupGrovePi.sh
+++ setupGrovePi.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install python-pip
 sudo pip install RPi.GPIO
 sudo pip install paho-mqtt
 git clone https://github.com/DexterInd/GrovePi 
-sudo cp /home/pi/GrovePi/Software/Python/grovepi.py /home/pi/gif_python
+sudo cp /home/pi/gif_python/GrovePi/Software/Python/grovepi.py /home/pi/gif_python
 cd GrovePi/Script
 sudo chmod +x install.sh
 sudo ./install.sh
