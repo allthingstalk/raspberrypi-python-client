43725d437a0a90c806d2e0caf7c89be200c93a00
Jan Bogaerts <jb@allthingstalk.com>
2015-03-04 15:04:27 +0100

    install script-> make certain in right dir

--- setupGrovePi.sh
+++ setupGrovePi.sh
@@ -1,8 +1,9 @@
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
 sudo pip install RPi.GPIO
+cd /home/pi/raspberrypi-python-client	#make certain that git clone is done frmo the right directory.
 sudo pip install paho-mqtt
-git clone https://github.com/DexterInd/GrovePi 
+git clone https://github.com/DexterInd/GrovePi 	
 sudo cp /home/pi/raspberrypi-python-client/GrovePi/Software/Python/grovepi.py /home/pi/raspberrypi-python-client
 sudo cp /home/pi/raspberrypi-python-client/GrovePi/Software/Python/grovepi.py /home/pi/raspberrypi-python-client/examples
 sudo cp /home/pi/raspberrypi-python-client/ATT_IOT.py /home/pi/raspberrypi-python-client/examples
