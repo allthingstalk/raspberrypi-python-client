ba567c4d289fea4a793e0a1fb2394bd0a7566001
Jan Bogaerts <jb@allthingstalk.com>
2016-02-29 15:15:23 +0100

    updates demo experiments for logging

--- experiments/shopWindow.py
+++ experiments/shopWindow.py
@@ -4,6 +4,8 @@
 # SmartLiving Shop Window experiment
 # Important: before running this demo, make certain that grovepi & ATT_IOT
 # are in the same directory as this script, or installed so that they are globally accessible
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
 import grovepi                                     #provides pin support
 import ATT_IOT as IOT  							   #provide cloud support
