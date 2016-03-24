5fcbf8b463b57ff2b09e8e87c3e1db79a3b5f87a
Jan Bogaerts <jb@allthingstalk.com>
2016-03-24 15:14:07 +0100

    update docs and package installer

--- ATT_IOT.py
+++ ATT_IOT.py
@@ -13,7 +13,7 @@ import logging
 
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
+def _on_connect(client, userdata, rc):
     if rc == 0:
         msg = "Connected to mqtt broker with result code "+str(rc)
         logging.info(msg)
@@ -22,21 +22,21 @@ def on_connect(client, userdata, rc):
             raise Exception("DeviceId not specified")
         topic = "client/" + ClientId + "/in/device/" + DeviceId + "/asset/+/command"
         logging.info("subscribing to: " + topic)
-        result = client.subscribe(topic)                                                    #Subscribing in on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
+        result = client.subscribe(topic)                                                    #Subscribing in _on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
         logging.info(str(result))                                                           # result is not a string on all platforms.
     else:
         logging.error("Failed to connect to mqtt broker: "  + mqtt.connack_string(rc))
 
 
 # The callback for when a PUBLISH message is received from the server.
-def on_MQTTmessage(client, userdata, msg):
+def _on_MQTTmessage(client, userdata, msg):
     payload = str(msg.payload)
     logging.info("Incoming message - topic: " + msg.topic + ", payload: " + payload)
     topicParts = msg.topic.split("/")
     if on_message is not None:
         on_message(topicParts[-2], msg.payload)										#we want the second last value in the array, the last one is 'command'
 
-def on_MQTTSubscribed(client, userdata, mid, granted_qos):
+def _on_MQTTSubscribed(client, userdata, mid, granted_qos):
     logging.info("Subscribed to topic, receiving data from the cloud: qos=" + str(granted_qos))
 
 
@@ -60,7 +60,10 @@ DeviceId = None
 ClientKey = None
 
 def connect(httpServer="api.smartliving.io", secure = False):
-    '''connect with the http server'''
+    '''create an HTTP connection with the server
+    :param httpServer: The dns name of the server to use for HTTP communication
+    :param secure: When true, an SSL connection will be used, if available.
+    '''
     global _httpClient#, _httpServerName                                         # we assign to these vars first, so we need to make certain that they are declared as global, otherwise we create new local vars
     if secure and socket.ssl:
         _httpClient = httplib.HTTPSConnection(httpServer)
@@ -70,13 +73,19 @@ def connect(httpServer="api.smartliving.io", secure = False):
     logging.info("connected with http server")
 
 def addAsset(id, name, description, isActuator, assetType, style = "Undefined"):
-    '''Add an asset to the device.
+    '''Create or update the specified asset. Call this function after calling 'connect' for each asset that you want to use.
     :param id: the local id of the asset
+    :type id: string or number
     :param name: the label that should be used to show on the website
+    :type name: basestring
     :param description: a description of the asset
+    :type description: basestring
     :param isActuator: True if this is an actuator. When False, it's created as a Sensor
+    :type isActuator: boolean
     :param assetType: the type of the asset, possible values: 'integer', 'number', 'boolean', 'text', None (defaults to string, when the asset already exists, the website will not overwrite any changes done manually on the site). Can also be a complete profile definition as a json string (see http://docs.smartliving.io/smartliving-maker/profiles/) example: '{"type": "integer", "minimum": 0}'.
+    :type assetType: string
     :param style: possible values: 'Primary', 'Secondary', 'Config', 'Battery'
+    :type style: basestring
     '''
 
     if not DeviceId:
@@ -106,7 +115,11 @@ def addAsset(id, name, description, isActuator, assetType, style = "Undefined"):
 
 
 def updateDevice(name, description, activityEnabled = False):
-    '''updates the definition of the device'''
+    '''updates the definition of the device
+    :param name: The name of the device
+    :param description: the description for the device
+    :param activityEnabled: When True, historical data will be stored on the db, otherwise only the last received value is stored.
+    '''
     global DeviceId
     if not DeviceId:
         raise Exception("DeviceId not specified")
@@ -125,8 +138,7 @@ def updateDevice(name, description, activityEnabled = False):
 
 def deleteDevice():
     '''
-        Deletes the currently loaded device from the cloud.  After this function, the global DeviceId will be reset
-        to None
+        Deletes the currently loaded device from the cloud.  After this function, the global DeviceId will be reset to None
     '''
     global DeviceId
     if not DeviceId:
@@ -149,12 +161,13 @@ def deleteDevice():
 
 def getPrimaryAsset():
     '''returns,as a list, the asset(s) assigned to the device as being "primary", that is, these assets represent the main functionality
-       of the device. Ex: a wall plug - powerswithch  can have many assets, but it's primary function is to switch on-off'''
+       of the device. Ex: a wall plug - powerswithch  can have many assets, but it's primary function is to switch on-off
+       :rtype: A json array contains all the assets that the cloud knows of for the current device and which have been labeled to be primary '''
     global DeviceId
     if not DeviceId:
         raise Exception("DeviceId not specified")
     url = "/Device/" + DeviceId + "/assets?style=primary"
-    return doHTTPGet(url, "")
+    return _doHTTPGet(url, "")
 
 
 def _buildPayLoadHTTP(value):
@@ -165,7 +178,10 @@ def _buildPayLoadHTTP(value):
 
 def sendValueHTTP(value, assetId):
     '''Sends a new value for an asset over http. This function is similar to send, accept that the latter uses mqtt
-       while this function uses HTTP'''
+       while this function uses HTTP
+
+       Parameters are the same as for the send function.
+       '''
     global DeviceId
     if not DeviceId:
         raise Exception("DeviceId not specified")
@@ -189,6 +205,10 @@ def sendCommandToHTTP(value, assetId):
         Note: you can only send commands to actuators that belong to devices in the same account as this device.
 
         ex: sendCommandTo('122434545abc112', 1)
+
+        :param value:same as for 'send' and 'sendValueHTTP'
+        :param assetId: the id of the asset to send the value to. This id must be the full id as found on the cloud app
+        :type assetId: basestring
     '''
     body = {"value": value }
     body = json.dumps(body)
@@ -205,24 +225,23 @@ def sendCommandToHTTP(value, assetId):
     jsonStr = response.read()
     logging.info(jsonStr)
 
-def getAssetState(asset):
+def getAssetState(asset, device = None):
     '''Gets the last recorded value for the specified asset.
-       When the asset is an int, it is presumed to be a local asset of this device. If it's a string, the asset
-       can be of an other device.
+       When device is ommitted (or None), the current device is used, otherwise the device with the specified id is used.
+    :param device: The id of the device to use. When None, the current device is queried.
+    :param asset: The id of the d
     :type asset: string or int
-    :returns a json object containing the last recorded data.
+    :return a json object containing the last recorded data.
     '''
-    if type(asset) == types.IntType:
-        global DeviceId
+    if not device:
         if not DeviceId:
             raise Exception("DeviceId not specified")
-        url = "/device/" + DeviceId + "/asset/" + str(asset) +  "/state"
-    else:
-        url = "/asset/" + asset + "/state"
-    return doHTTPGet(url, "")
+        device = DeviceId
+    url = "/device/" + device + "/asset/" + str(asset) + "/state"
+    return _doHTTPGet(url, "")
 
 
-def doHTTPGet(url, content):
+def _doHTTPGet(url, content):
     headers = {"Content-type": "application/json", "Auth-ClientKey": ClientKey, "Auth-ClientId": ClientId}
     logging.info("HTTP GET: " + url)
     logging.info("HTTP HEADER: " + str(headers))
@@ -242,19 +261,26 @@ def doHTTPGet(url, content):
 def getAssets():
     '''
         gets the list of assets that are known for this device in the cloud
-        :returns a json array containing all the assets.
+        :return a json array containing all the assets.
     '''
     global DeviceId
     if not DeviceId:
         raise Exception("DeviceId not specified")
     url = "/Device/" + DeviceId + "/assets"
 
-    return doHTTPGet(url, "")
+    return _doHTTPGet(url, "")
 
-def subscribe(mqttServer = "broker.smartliving.io", port = 1883, secure = False):
-    '''start the mqtt client and make certain that it can receive data from the IOT platform
-	   mqttServer: (optional): the address of the mqtt server. Only supply this value if you want to a none standard server.
-	   port: (optional) the port number to communicate on with the mqtt server.
+def subscribe(mqttServer = "broker.smartliving.io", port = 1883, secure = False, certFile = 'cacert.pem'):
+    '''Sets up everything for the pub-sub client: create the connection, provide the credentials and register for any possible incoming data.
+	   :param mqttServer:  the address of the mqtt server. Only supply this value if you want to a none standard server. Default = broker.smartliving.io
+	   :param port: the port number to communicate on with the mqtt server. Default = 1883
+	   :param secure: When true, an SSL connection is used. Default = False.  When True, use port 8883 on broker.smartliving.io
+	   :param certFile: certfile is a string pointing to the PEM encoded client
+        certificate and private keys respectively. Note
+        that if either of these files in encrypted and needs a password to
+        decrypt it, Python will ask for the password at the command line. It is
+        not currently possible to define a callback to provide the password.
+	   Note: SSL will can only be used when the mqtt lib has been compiled with support for ssl
     '''
     global _mqttClient, _httpClient                                             # we assign to these vars first, so we need to make certain that they are declared as global, otherwise we create new local vars
     _httpClient.close()
@@ -264,16 +290,16 @@ def subscribe(mqttServer = "broker.smartliving.io", port = 1883, secure = False)
     else:
         mqttId = DeviceId
     _mqttClient = mqtt.Client(mqttId)
-    _mqttClient.on_connect = on_connect
-    _mqttClient.on_message = on_MQTTmessage
-    _mqttClient.on_subscribe = on_MQTTSubscribed
+    _mqttClient.on_connect = _on_connect
+    _mqttClient.on_message = _on_MQTTmessage
+    _mqttClient.on_subscribe = _on_MQTTSubscribed
     if ClientId is None:
         logging.info("ClientId not specified, can't connect to broker")
         raise Exception("ClientId not specified, can't connect to broker")
     brokerId = ClientId + ":" + ClientId
     _mqttClient.username_pw_set(brokerId, ClientKey)
     if secure and socket.ssl:
-        _mqttClient.tls_set("cacert.pem")
+        _mqttClient.tls_set(certFile)
     _mqttClient.connect(mqttServer, port, 60)
     _mqttClient.loop_start()
 
@@ -288,6 +314,14 @@ def _buildPayLoad(value):
 	
 	
 def send(value, assetId):
+    """
+    send a data value to the cloud server, using MQTT, for the asset with the specified id as provided by the IoT platform.
+    :param value: the value to send. This can be in the form of a string, int, double, bool or python object/list All primitive values are converted to a lower case string, ex: 'true' or 'false'
+You can also send an object or a python list with this function to the cloud. Objects will be converted to json objects, lists become json arrays. The fields/records in the json objects or arrays must be the same as defined in the profile.
+    :type value: number, string, boolean, object or list
+    :param assetId: the id of the asset to send the value to, usually the pin number. This is the local id that you used while creating/updating the asset through the function 'addAsset' ex: 1
+    :type assetId: string or number
+    """
     if ClientId is None:
         logging.error("ClientId not specified")
         raise Exception("ClientId not specified")
