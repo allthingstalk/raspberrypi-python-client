d9b4f664cc8c3f7b483ef83f8c3c2130a1b1f352
Sander Hendrickx <shendrickx@hotmail.com>
2017-03-15 14:28:23 +0100

    Update nw_watchdog.py
    update watchdog comments

--- att_iot_client/nw_watchdog.py
+++ att_iot_client/nw_watchdog.py
@@ -21,13 +21,13 @@ import ATT_IOT as IOT
 import datetime
 import logging
 
-#callback when the ping failed. Allows the client to perform special actions. Can be used to reboot the system
+# callback when the ping failed. Allows the client to perform special actions. Can be used to reboot the system
 # the callback is called before trying to restart the mqtt connection.
 # no parameters are used, so the signagature = def on_failaure()
 on_failure = None
 
-WatchDogAssetId = -1    #the asset id used by the watchdog. Change this if it interfers with your own asset id's.
-PingFrequency = 3000    #the frequency in seconds, that a ping is sent out (and that the system expects a ping back)
+WatchDogAssetId = -1    # the asset id used by the watchdog. Change this if it interfers with your own asset id's.
+PingFrequency = 3000    # the frequency in seconds, that a ping is sent out (and that the system expects a ping back)
 
 
 _nextPingAt = None      # the moment in time that the next ping should be sent.
@@ -35,13 +35,17 @@ _pingCounter = 0        # the count of the ping, increments every time a ping is
 _lastReceived = 0       # the ping counter that was last received.
 
 def ping():
-    """send a ping to the server"""
+    """
+    Send a ping to the server
+    """
     global _nextPingAt
     _nextPingAt = datetime.datetime.now() + datetime.timedelta(0, PingFrequency)
     IOT.sendCommandTo(_pingCounter, IOT.DeviceId, WatchDogAssetId)
 
 def checkPing():
-    """check if we need to resend a ping and if we received the previous ping in time"""
+    """
+    Check if we need to resend a ping and if we received the previous ping in time
+    """
     global _pingCounter
     if _nextPingAt <= datetime.datetime.now():
         if _lastReceived != _pingCounter:
@@ -57,9 +61,9 @@ def checkPing():
     return True
 
 def isWatchDog(id, value):
-    """check if an incomming command was a ping from the watchdog
-    :returns: if the id was that of the watchdog.
-    :rtype: bool
+    """
+    Check if an incomming command was a ping from the watchdog
+    :return: `bool` True if the id was that of the watchdog, else False
     """
     global _lastReceived
     if id == str(WatchDogAssetId):
@@ -70,6 +74,6 @@ def isWatchDog(id, value):
 
 def setup():
     """
-	Add the watchdog asset to the device.  This can be used as visual feedback for the state of the device.
-	"""
+    Add the watchdog asset to the device.  This can be used as visual feedback for the state of the device.
+    """
     IOT.addAsset(WatchDogAssetId, "network watchdog", "used to verify the connectivity with the broker", True, "integer")
